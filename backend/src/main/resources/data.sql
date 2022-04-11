b38c80e39022b78a3dc2a637b29d3af722705f98
Daniel Xavier <dxavier194@gmail.com>
2022-04-11 14:05:25 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png', 'https://cdn.iconscout.com/icon/free/png-256/html-261-761679.png');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-11-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-11-13T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-12-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-12-13T03:00:00Z', 1);
